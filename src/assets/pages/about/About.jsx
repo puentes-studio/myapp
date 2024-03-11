0002ad308edf68655802852d31a69bcbd458f529
Vlad Beltran <puentes.studio@gmail.com>
2024-03-11 17:35:26 +0100

    styles and about

--- src/assets/pages/about/About.jsx
+++ src/assets/pages/about/About.jsx
@@ -12,24 +12,29 @@ function About() {
     <>
       <Nav />
       <div>
-        <a href="#" target="_blank">
-          <img src={vladphoto} className="cv-photo" alt="CV Photo" />
-        </a>
+        <img src={vladphoto} className="cv-photo" alt="CV Photo" />
       </div>
-      <h1 className="about-title">Vlad Beltran</h1>
-      {/* <div className="card">
+      <div>
+        <h1 className="about-title">Vlad Beltran</h1>
+        {/* <div className="card">
         <button onClick={() => setCount(count + 1)}>count is {count}</button>
       </div> */}
-      <p className="description-about">
-        I'm a <span className="highlight-1">Front-End Developer</span>{" "}
-        proficient and goal focused, dedicated to crafting immersive user
-        experiences. With a{" "}
-        <span className="highlight-2">background in graphic design</span> and{" "}
-        <span className="highlight-3">
-          passion for learning, technology and innovation,
-        </span>
-        I bring a blend of creativity and technical expertise to every project.
-      </p>
+        <p className="description-about">
+          I'm a <span className="highlight-1">Front-End Developer</span>{" "}
+          proficient and goal focused, dedicated to crafting immersive user
+          experiences. With a{" "}
+          <span className="highlight-2">background in graphic design</span> and{" "}
+          <span className="highlight-3">
+            passion for learning, technology and innovation,
+          </span>{" "}
+          I bring a blend of creativity and technical expertise to every
+          project.
+        </p>
+      </div>
+      <section className="tech-stack">
+        <h3>Technology stack</h3>
+        <p>React, Node.js, Git, SQL, C#, ES6, HTML, CSS, Adobe, Figma.</p>
+      </section>
     </>
   );
 }
