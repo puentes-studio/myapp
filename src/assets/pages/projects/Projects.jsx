c13b2fc33dade85d4e71d3a1168684899ba018a2
Vlad Beltran <puentes.studio@gmail.com>
2024-03-13 18:38:29 +0100

    projects list done

--- src/assets/pages/projects/Projects.jsx
+++ src/assets/pages/projects/Projects.jsx
@@ -1,8 +1,11 @@
 import "./Projects.css";
 import unika from "../../img/projects/unika.png";
+import puentesStudio from "../../img/projects/logo_puentes_studio.png";
+import pokemon from "../../img/projects/pokemon.webp";
+import drumpadd from "../../img/projects/DrumApp.png";
 import Nav from "../../../components/nav/Nav";
 import Footer from "../../../components/footer/Footer";
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Projects() {
   return (
@@ -26,13 +29,18 @@ function Projects() {
               </figure>
               <div className="project-info">
                 <h3 className="project-title">Unika HR</h3>
-                <Link
-                  className="project-website"
-                  target="blank"
-                  to="https://unikahr.com/"
-                >
-                  www.unikahr.com
-                </Link>
+                <div className="links-container">
+                  <Link
+                    className="project-website"
+                    target="blank"
+                    to="https://unikahr.com/"
+                  >
+                    See project
+                  </Link>
+                  <Link className="project-website" target="blank" to="">
+                    See Repository
+                  </Link>
+                </div>
               </div>
             </li>
             <li className="project-container">
@@ -42,13 +50,26 @@ function Projects() {
                 className="project-link"
               ></NavLink> */}
               <figure className="project-logo">
-                <img src={unika} className="logo-client" alt="Project-logo" />
+                <img
+                  src={puentesStudio}
+                  className="logo-client"
+                  alt="Project-logo"
+                />
               </figure>
               <div className="project-info">
-                <h3 className="project-title">Unika HR</h3>
-                <a className="project-website" href="https://unikahr.com/">
-                  www.unikahr.com
-                </a>
+                <h3 className="project-title">Puentes Studio</h3>
+                <div className="links-container">
+                  <Link
+                    className="project-website"
+                    target="blank"
+                    to="https://puentesstudio.com/"
+                  >
+                    See project
+                  </Link>
+                  <Link className="project-website" target="blank" to="">
+                    See Repository
+                  </Link>
+                </div>
               </div>
             </li>
             <li className="project-container">
@@ -58,13 +79,26 @@ function Projects() {
                 className="project-link"
               ></NavLink> */}
               <figure className="project-logo">
-                <img src={unika} className="logo-client" alt="Project-logo" />
+                <img src={pokemon} className="logo-client" alt="Project-logo" />
               </figure>
               <div className="project-info">
-                <h3 className="project-title">Unika HR</h3>
-                <a className="project-website" href="https://unikahr.com/">
-                  www.unikahr.com
-                </a>
+                <h3 className="project-title">Pokedesk</h3>
+                <div className="links-container">
+                  <Link
+                    className="project-website"
+                    target="blank"
+                    to="https://pokemon-black-sable.vercel.app/"
+                  >
+                    See Project
+                  </Link>
+                  <Link
+                    className="project-website"
+                    target="blank"
+                    to="https://github.com/puentes-studio/PokemonBlack"
+                  >
+                    See Repository
+                  </Link>
+                </div>
               </div>
             </li>
             <li className="project-container">
@@ -74,13 +108,30 @@ function Projects() {
                 className="project-link"
               ></NavLink> */}
               <figure className="project-logo">
-                <img src={unika} className="logo-client" alt="Project-logo" />
+                <img
+                  src={drumpadd}
+                  className="logo-client"
+                  alt="Project-logo"
+                />
               </figure>
               <div className="project-info">
-                <h3 className="project-title">Unika HR</h3>
-                <a className="project-website" href="https://unikahr.com/">
-                  www.unikahr.com
-                </a>
+                <h3 className="project-title">Drummpadd</h3>
+                <div className="links-container">
+                  <Link
+                    className="project-website"
+                    target="blank"
+                    to="https://pokemon-black-sable.vercel.app/"
+                  >
+                    See Project
+                  </Link>
+                  <Link
+                    className="project-website"
+                    target="blank"
+                    to="https://github.com/puentes-studio/PokemonBlack"
+                  >
+                    See Repository
+                  </Link>
+                </div>
               </div>
             </li>
             <li className="project-container">
@@ -90,17 +141,30 @@ function Projects() {
                 className="project-link"
               ></NavLink> */}
               <figure className="project-logo">
-                <img src={unika} className="logo-client" alt="Project-logo" />
+                <img
+                  src={drumpadd}
+                  className="logo-client"
+                  alt="Project-logo"
+                />
               </figure>
               <div className="project-info">
-                <h3 className="project-title">Unika HR</h3>
-                <Link
-                  className="project-website"
-                  target="blank"
-                  href="https://unikahr.com/"
-                >
-                  www.unikahr.com
-                </Link>
+                <h3 className="project-title">Drummpadd</h3>
+                <div className="links-container">
+                  <Link
+                    className="project-website"
+                    target="blank"
+                    to="https://pokemon-black-sable.vercel.app/"
+                  >
+                    See Project
+                  </Link>
+                  <Link
+                    className="project-website"
+                    target="blank"
+                    to="https://github.com/puentes-studio/PokemonBlack"
+                  >
+                    See Repository
+                  </Link>
+                </div>
               </div>
             </li>
           </ul>
