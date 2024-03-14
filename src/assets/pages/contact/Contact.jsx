b55366b2ae8af518e1cf4c9dfde69cc334830ade
Vlad Beltran <puentes.studio@gmail.com>
2024-03-14 14:36:43 +0100

    form in contact page

--- src/assets/pages/contact/Contact.jsx
+++ src/assets/pages/contact/Contact.jsx
@@ -1,31 +1,12 @@
 import "./Contact.css";
 import Nav from "../../../components/nav/Nav";
 import Footer from "../../../components/footer/Footer";
+import ContactForm from "../../../components/contactForm/ContactForm";
+import PhoneLink from "../../../components/phoneLink/PhoneLink";
 // import { useName } from "../../../../Context/Name.Context";
-import { useState } from "react";
 
 function Contact() {
-  // const [count, setCount] = useState(0);
   // const { nameState } = useName();
-
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    description: "",
-  });
-
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Handle form submission here
-    console.log(formData);
-  };
 
   return (
     <>
@@ -40,45 +21,8 @@ function Contact() {
       </div>
 
       <div className="form-container">
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="name">Name:</label>
-          <br />
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
-          <br />
-
-          <label htmlFor="email">Email:</label>
-          <br />
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-          <br />
-
-          <label htmlFor="description">Description:</label>
-          <br />
-          <textarea
-            id="description"
-            name="description"
-            value={formData.description}
-            onChange={handleChange}
-            rows="4"
-            required
-          ></textarea>
-          <br />
-
-          <button type="submit">Submit</button>
-        </form>
+        <ContactForm />
+        <PhoneLink />
       </div>
 
       <Footer />
---
+++ src/components/phoneLink/PhoneLink.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function PhoneLink() {
+  return (
+    <div>
+      <h2>Click to Call</h2>
+      <a href="tel:+34658608216">Call Us: +34 658 608 216</a>
+      <a href="tel:+447515482792">Call Us: +44 7515 482 792</a>
+    </div>
+  );
+}
+
+export default PhoneLink;
